bb37d736a0251fc2f02d44eb3ae5a5d8aeb644cf
Tathagata Mishra <tathagatamishra007@gmail.com>
2025-04-12 00:42:43 +0530

    a2 jp web ui

--- component/A2/A2.js
+++ component/A2/A2.js
@@ -6,9 +6,9 @@ import { CiInstagram, CiMail } from "react-icons/ci";
 
 export default function A2() {
   return (
-    <div className="w-full h-fit min-h-screen p-[30px]">
-      <div className="w-full">
-        <nav className="w-full px-[20px] py-[20px] border-[4px] border-[#797979] rounded-[10px] flex flex-row justify-between">
+    <div className="w-full h-fit min-h-screen p-[30px] flex flex-col gap-[30px]">
+      <div className="relative w-full">
+        <nav className="z-[2] relative w-full px-[20px] py-[20px] border-[4px] border-[#797979] rounded-[10px] flex flex-row justify-between">
           <p className="logo flex flex-col justify-center leading-[20px] text-[40px] text-[#797979] font-[800] tracking-[10px]">
             tette
           </p>
@@ -42,12 +42,57 @@ export default function A2() {
           </div>
         </nav>
 
-        <div></div>
+        <div className="z-[3] relative w-full h-[30px] flex flex-row justify-between items-center">
+          <div className="z-[1] h-[38px] w-[10%] border-[4px] border-l-0 border-[#797979] rounded-r-[10px] absolute left-[10px]"></div>
+
+          <div className="z-[0] h-[38px] w-[40%] absolute left-[4%] bg-[#ffffff]"></div>
+
+          <div className="z-[1] h-[38px] w-[80%] border-[4px] border-r-0 border-[#797979] rounded-l-[10px] absolute right-[10px]"></div>
+        </div>
+
+        <div className="z-[2] relative w-full px-[20px] py-[20px] border-[4px] border-[#797979] rounded-[10px]">
+          <Image
+            src="https://fastly.picsum.photos/id/325/4928/3264.jpg?hmac=D_X6AKqCcH8IpWElX5X3dxx11yn7yYO-vPhiKhzRbwI"
+            alt=""
+            width={4928}
+            height={3264}
+            className="w-full h-[35vw] object-cover"
+          />
+        </div>
       </div>
 
-      <div></div>
+      <div className="w-full flex flex-row gap-[24px]">
+        <div className="w-full h-[200px] px-[20px] py-[20px] border-[4px] border-[#797979] rounded-[10px] flex flex-col justify-center items-center">
+          推薦的
+        </div>
+        <div className="w-full h-[200px] px-[20px] py-[20px] border-[4px] border-[#797979] rounded-[10px] flex flex-col justify-center items-center">
+          推薦的
+        </div>
+        <div className="w-full h-[200px] px-[20px] py-[20px] border-[4px] border-[#797979] rounded-[10px] flex flex-col justify-center items-center">
+          推薦的
+        </div>
+        <div className="w-full h-[200px] px-[20px] py-[20px] border-[4px] border-[#797979] rounded-[10px] flex flex-col justify-center items-center">
+          推薦的
+        </div>
+      </div>
 
-      <div></div>
+      <div className="relative w-full">
+        <nav className="z-[2] relative w-full px-[20px] py-[20px] border-[4px] border-[#797979] rounded-[10px] flex flex-row justify-between">
+
+        </nav>
+
+        <div className="z-[3] relative w-full h-[30px] flex flex-row justify-between items-center">
+          <div className="z-[1] h-[38px] w-[10%] border-[4px] border-l-0 border-[#797979] rounded-r-[10px] absolute left-[10px]"></div>
+
+          <div className="z-[0] h-[38px] w-[40%] absolute left-[4%] bg-[#ffffff]"></div>
+
+          <div className="z-[1] h-[38px] w-[80%] border-[4px] border-r-0 border-[#797979] rounded-l-[10px] absolute right-[10px]"></div>
+        </div>
+
+        <div className="z-[2] relative w-full px-[20px] py-[20px] border-[4px] border-[#797979] rounded-[10px]">
+
+        </div>
+      </div>
     </div>
   );
 }
